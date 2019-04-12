737fade76397b0740bed4b8cf0bc887e71fb49e7
nitanmarcel <nitan.marcel@gmail.com>
2019-04-12 18:18:46 +0300

    Update to 2.1

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 import sys
 
-import sys
 if not sys.version_info[0] == 3:
     sys.exit("Sorry, only Python 3 is supported at this moment\n If you are using python3 and you get this message try to install the lib with pip3")
 
 setup(
    name='fancy_text',
-   version='2.0',
+   version='2.1',
    description='Generate fancy text',
+   long_description='Generate fancy text',
    author='Marcel Alexandru Nitan',
    author_email='nitan.marcel@gmail.com',
    license='MIT',
